fd46a99adacc443457c7272257de12920e24ec9b
Samuel Simplicio <samuelsimplicio5@gmail.com>
2025-08-22 00:02:32 -0300

    Separate tests into files

--- src/tests.zig
+++ src/tests.zig
@@ -10,36 +10,42 @@ test "math ops" {
     }
 
     const allocator = gpa.allocator();
-    var file = try std.fs.cwd().openFile("src/test-files/math.lisp", .{});
-    defer file.close();
 
-    const file_size = try file.getEndPos();
-    const buffer = try allocator.alloc(u8, file_size);
-    defer allocator.free(buffer);
+    for ([_][]const u8{
+        "src/test-files/eval.lisp",
+        "src/test-files/if_fn_do.lisp",
+    }) |filename| {
+        var file = try std.fs.cwd().openFile(filename, .{});
+        defer file.close();
 
-    _ = try file.readAll(buffer);
+        const file_size = try file.getEndPos();
+        const buffer = try allocator.alloc(u8, file_size);
+        defer allocator.free(buffer);
 
-    var interpreter = Interpreter.init(allocator);
-    defer interpreter.deinit();
+        _ = try file.readAll(buffer);
 
-    var it = std.mem.splitScalar(u8, buffer, '\n');
-    var i: usize = 0;
-    while (it.next()) |line| {
-        i += 1;
-        if (line.len == 0) {
-            continue;
+        var interpreter = Interpreter.init(allocator);
+        defer interpreter.deinit();
+
+        var it = std.mem.splitScalar(u8, buffer, '\n');
+        var i: usize = 0;
+        while (it.next()) |line| {
+            i += 1;
+            if (line.len == 0) {
+                continue;
+            }
+
+            const ret = interpreter.re(line) catch {
+                std.debug.print("ERR file {s} line {}: {s}\n", .{ filename, i, interpreter.err_ctx.buffer.items });
+                try std.testing.expect(false);
+                return;
+            };
+            std.testing.expect(ret.eql(LispType.lisp_true)) catch {
+                const str = try ret.toStringFull(allocator);
+                defer allocator.free(str);
+                std.debug.print("ERR file {s} line {}: return value {s}\n", .{ filename, i, str });
+                try std.testing.expect(false);
+            };
         }
-
-        const ret = interpreter.re(line) catch {
-            std.debug.print("ERR line {}: {s}\n", .{ i, interpreter.err_ctx.buffer.items });
-            try std.testing.expect(false);
-            return;
-        };
-        std.testing.expect(ret.eql(LispType.lisp_true)) catch {
-            const str = try ret.toStringFull(allocator);
-            defer allocator.free(str);
-            std.debug.print("ERR line {}: return value {s}\n", .{ i, str });
-            try std.testing.expect(false);
-        };
     }
 }
