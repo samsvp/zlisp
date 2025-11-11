efd7220270ffd71b9cdf47a2732817b640041db7
Samuel Simplicio <samuelsimplicio5@gmail.com>
2025-11-10 23:28:26 -0300

    Start making the compile list

--- src/frontend/compiler.zig
+++ src/frontend/compiler.zig
@@ -7,10 +7,36 @@ const Value = @import("../value.zig").Value;
 const Chunk = @import("../backend/chunk.zig").Chunk;
 const OpCode = @import("../backend/chunk.zig").OpCode;
 
-pub fn compileAtom(allocator: std.mem.Allocator, chunk: *Chunk, atom: reader.Token.Atom) !void {
-    switch (atom.value) {
-        else => try chunk.emitConstant(allocator, atom.value, atom.line),
+pub const Errors = error{
+    NonFunctionAsHeadOfList,
+};
+
+pub fn compileAtom(allocator: std.mem.Allocator, chunk: *Chunk, value: Value, line: usize) !void {
+    try chunk.emitConstant(allocator, value, line);
+}
+
+pub fn compileList(
+    allocator: std.mem.Allocator,
+    chunk: *Chunk,
+    list: std.ArrayList(reader.Token),
+    line: usize,
+    err_ctx: *errors.Ctx,
+) !void {
+    if (list.items.len == 0) {
+        const empty_list = try Obj.List.empty(allocator);
+        try chunk.emitConstant(allocator, .{ .obj = &empty_list.obj }, line);
+        return;
     }
+
+    const first = list.items[0];
+    if (first.kind != .atom) {
+        // TODO! The first element could be a list if it defines an anonymous function
+        err_ctx.line = line;
+        err_ctx.setMsg(allocator, "COMPILE", "List first element must be function.", .{});
+        return Errors.NonFunctionAsHeadOfList;
+    }
+
+    // TODO! the rest of the function, lol
 }
 
 pub fn compile(allocator: std.mem.Allocator, source: []const u8, err_ctx: *errors.Ctx) !*Chunk {
@@ -20,26 +46,21 @@ pub fn compile(allocator: std.mem.Allocator, source: []const u8, err_ctx: *error
     var tokens = try reader.readStr(allocator, source, err_ctx);
     defer tokens.deinit(allocator);
 
-    for (tokens.items, 0..) |token, i| switch (token) {
+    for (tokens.items, 0..) |token, i| switch (token.kind) {
         .atom => |a| {
             if (i != tokens.items.len - 1) {
                 // ignore any atom that is not the last statement
                 continue;
             }
-            try compileAtom(allocator, chunk, a);
+            try compileAtom(allocator, a, token.line);
         },
         .list => |list| {
-            if (list.items.len == 0) {
-                if (i != tokens.items.len - 1) {
-                    // ignore any atom that is not the last statement
-                    continue;
-                }
-
-                // return empty list
-                const empty_list = try Obj.List.empty(allocator);
-                try chunk.emitConstant(allocator, .{ .obj = &empty_list.obj }, 0);
+            if (list.items.len == 0 and i != tokens.items.len - 1) {
+                // ignore any atom that is not the last statement
                 continue;
             }
+
+            compileList(allocator, chunk, list, token.line);
         },
     };
 
